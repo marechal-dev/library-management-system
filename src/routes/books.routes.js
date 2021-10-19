91e6afaa6eb60f624d81a44fc4882e9974258d21
marechal-dev <pietro.developer@gmail.com>
2021-10-19 10:08:02 -0300

    refactor(middleware): Refactor verifyIfBookExists middleware

--- src/routes/books.routes.js
+++ src/routes/books.routes.js
@@ -1,52 +1,17 @@
 const { Router } = require('express')
-const { v4 } = require('uuid')
+
+const BookController = require('../controllers/BookController')
+
+const { verifyIfBookExists } = require('../middlewares/verifyIfBookExists')
 
 const booksRoutes = Router()
 
-const books = []
+booksRoutes.get('/', BookController.getBooks)
 
-function verifyIfBookExists(req, res, next) {
-  const { id } = req.params
+booksRoutes.post('/', BookController.postBook)
 
-  const book = books.find((book) => book.id === id)
+booksRoutes.put('/:id', verifyIfBookExists, BookController.putBook)
 
-  if(!book) {
-    return res.status(404).json({ error: "Book doesn't exists!" })
-  }
-
-  return next()
-}
-
-booksRoutes.get('/', (req, res) => {
- // Done
-})
-
-booksRoutes.post('/', (req, res) => {
-  // Done
-})
-
-booksRoutes.put('/:id', verifyIfBookExists, (req, res) => {
-  const { id } = req.params
-  const { title, publisher, cover, authors } = req.body
-
-  const book = books.find(book => book.id === id)
-
-  book.title = title
-  book.publisher = publisher
-  book.cover = cover
-  book.authors = authors
-
-  return res.status(200).json(book)
-})
-
-booksRoutes.delete('/:id', verifyIfBookExists, (req, res) => {
-  const { id } = req.params
-
-  const book = books.indexOf(book => book.id === id)
-
-  books.splice(book, 1)
-
-  return res.status(200).json(books)
-})
+booksRoutes.delete('/:id', verifyIfBookExists, BookController.deleteBook)
 
 module.exports = booksRoutes
